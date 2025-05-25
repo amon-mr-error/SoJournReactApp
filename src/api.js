5e17f70419c360c38135278b646310c541baec76
Amon <kes.karna12@gmail.com>
2025-05-25 17:16:53 +0530

    Added the backend url

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 //process.env.REACT_APP_API_BASE ||
 
-const API_BASE =  "";
+const API_BASE =  process.env.REACT_APP_API_BASE;
 
 export const getToken = () => localStorage.getItem("token");
 export const setToken = (token) => localStorage.setItem("token", token);
