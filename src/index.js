8d8f831030bedc358397b7b521add220ec749630
SafalBhandari12 <safalbhandari069@gmail.com>
2025-08-18 18:40:47 +0530

    changed for fun

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 // import React from 'react';
 // import ReactDOM from 'react-dom/client';
-import './index.css';
+import "./index.css";
 // import { BrowserRouter } from "react-router-dom";
 // import App from './App';
-
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(
@@ -22,7 +21,7 @@ const root = createRoot(container);
 
 root.render(
   <BrowserRouter>
-    <App />
-    {/* <h1>Hello World</h1> */}
+    {/* <App /> */}
+    <h1></h1>
   </BrowserRouter>
 );
